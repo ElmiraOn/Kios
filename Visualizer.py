48ecd5f01dc02885bd4ea43458851c68f9198962
ElmiraOn <elmiraonagh@yahoo.com>
2023-05-01 21:43:35 -0400

    init

---
+++ Visualizer.py
@@ -0,0 +1,62 @@
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import datetime
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+st.set_page_config(layout="wide")
+local_css("style.css")
+
+
+df = pd.read_csv('Book1.csv')
+col1, col2 = st.columns(2)
+
+
+def invalid_purchase():
+    #send data to the model and return value
+    model_result = True # it is fraud
+    current_time = datetime.datetime.now()
+    new_row = pd.DataFrame({'date':current_time ,'vendor': 'ALDO', 'amout': "$200"},index =[0])
+    global df
+    df = pd.concat([new_row, df]).reset_index(drop = True)
+    color = (df.vendor == 'ALDO').map({True: 'background-color: yellow', False: ''})
+    df.style.apply(lambda s: color)
+
+    return model_result
+
+def valid_purchase():
+    #send data to the model and return value
+
+    current_time = datetime.datetime.now()
+    new_row = pd.DataFrame({'date':current_time ,'vendor': "ALDO", 'amout': "$200"},index =[0])
+    global df
+    df = pd.concat([new_row, df]).reset_index(drop = True)
+
+fraud_detected = False
+
+with col1:
+    with st.container():
+        st.header("valid tranaction")
+        image = Image.open('shoes.jpg')
+        st.image(image )
+        btn1 = st.button("Purchase", key="valid_purch", use_container_width=True)
+        if btn1:
+            valid_purchase()
+    with st.container():
+        st.header("Fraud transaction") 
+        image = Image.open('shoes.jpg')
+        st.image(image )
+        btn2 = st.button("Purchase",key="invalid_purch", use_container_width=True)
+        if btn2:
+            fraud_detected = invalid_purchase()
+
+with col2: 
+    with st.container():
+        st.header("FI view")
+        st.subheader("John Doe's transaction history")
+        if(fraud_detected):
+            st.write("Fraud detected!")
+        st.dataframe(df, use_container_width=True)
+        
